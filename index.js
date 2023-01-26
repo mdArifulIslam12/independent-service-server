0e03ea18f293d3d36fb3bf80ffb956649b5da69e
mdArifulIslam12 <mdarif767778@gmail.com>
2023-01-26 19:35:37 +0600

    travel-server

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app =  express()
+require("dotenv").config();
 const cors = require('cors');
-const port = 5000
+const port = process.env.PORT ||  5000
 
 app.use(cors())
 app.use(express.json())
